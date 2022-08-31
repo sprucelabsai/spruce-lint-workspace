a026a30925af192534d86f3a0f2a1eb3dc2e213b
Ryan Johnson <ryan@sprucelabs.ai>
2022-08-31 08:13:15 -0400

    patch: missed file

--- packages/eslint-test-spruce/lint-samples/pass-decorators.ts
+++ packages/eslint-test-spruce/lint-samples/pass-decorators.ts
@@ -1,4 +1,4 @@
 import { fake } from '@sprucelabs/spruce-test-fixtures'
 
 @fake.login()
-export default class RegisteringTest extends AbstractTest {}
+export default class NoUnusedVarsDecoratorTest {}
